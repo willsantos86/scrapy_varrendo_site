32b989fe3c6c38925893f39ee8bff8002eb6c48f
William Santos <willsantos.edf@gmail.com>
2022-12-22 01:58:56 -0300

    Adicionando banco de dados.

--- varredor_de_sites/spiders/proxylist.py
+++ varredor_de_sites/spiders/proxylist.py
@@ -12,9 +12,9 @@ class ProxyScraperSpider(scrapy.Spider):
     def parse(self,response):
         for linha in response.xpath("//table[@class='table table-striped table-bordered']//tr"):
             yield {
-                'Proxy Name': linha.xpath('./td/a/text()').get(),
+                'Proxy Name': linha.xpath('./td[1]/a/text()').get(),
                 'Domain': linha.xpath('./td[2]/text()').get(),
                 'Country': linha.xpath('./td[3]/text()').get(),
                 'Speed': linha.xpath('./td[4]/text()').get(),
-                'Popularity': linha.xpath('./td/div/div/text()').get()
+                'Popularity': linha.xpath('./td[5]/div/div/text()').get()
             }
--- varredor_de_sites/spiders/proxyscraper.py
+++ varredor_de_sites/spiders/proxyscraper.py
@@ -6,20 +6,21 @@ class ProxyScraperSpider(scrapy.Spider):
     #Request
     def start_requests(self):
         urls = ['https://www.us-proxy.org/']
+
         for url in urls:
-            yield scrapy.Request(url=url,callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse, meta={'next_url':urls[0]})
     #Response
-    def parse(self,response):
+    def parse(self, response):
         #Montar um xath para retornar a linha
-        for linha in response.xpath("//table[@class='table table-striped table-bordered']//tr"):
+        for quote in response.xpath("//table[@class='table table-striped table-bordered']//tr"):
             yield {
-                'IP Address': linha.xpath('./td[1]/text()').get(),
-                'Port': linha.xpath('./td[2]/text()').get(),
-                'Code': linha.xpath('./td[3]/text()').get(),
-                'Country': linha.xpath('./td[4]/text()').get(),
-                'Anonymity': linha.xpath('./td[5]/text()').get(),
-                'Google': linha.xpath('./td[6]/text()').get(),
-                'Google': linha.xpath('./td[7]/text()').get(),
-                'Last Checked': linha.xpath('./td[8]/text()').get()
+                'ip_address': quote.xpath('./td[1]/text()').get(),
+                'port': quote.xpath('./td[2]/text()').get(),
+                'code': quote.xpath('./td[3]/text()').get(),
+                'country': quote.xpath('./td[4]/text()').get(),
+                'anonimity': quote.xpath('./td[5]/text()').get(),
+                'google': quote.xpath('./td[6]/text()').get(),
+                'https': quote.xpath('./td[7]/text()').get(),
+                'last_checked': quote.xpath('./td[8]/text()').get(),
 
             }
